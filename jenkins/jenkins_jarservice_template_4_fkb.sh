a182a2f12f2ddc7298124fdb39fb3c4621521c7d
niwei_notebook <yodoo_admin@163.com>
2019-10-08 16:30:50 +0800

    add fkb  service start template

---
+++ jenkins/jenkins_jarservice_template_4_fkb.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+companyname="yodoo"
+jarfullname="wbzChannel.jar"
+pidname="wbzChannel-16400"
+logdir="/opt/log"
+servicedir="/opt/service"
+
+
+(
+cat <<EOF
+#!/bin/sh
+
+sudo mkdir -p /var/run/$companyname
+sudo chown -R admin:admin /var/run/yodoo
+nohup java -jar $jarfullname > $logdir/$pidname.log 2>&1 &
+echo \$! > /var/run/$companyname/$pidname.pid
+
+EOF
+) > $servicedir/$pidname-start.sh
+
+
+(
+cat <<EOF
+#!/bin/sh
+
+PID=\$(cat /var/run/$companyname/$pidname.pid)
+kill -9 \$PID
+EOF
+) > $servicedir/$pidname-stop.sh
+
+
+(
+cat <<EOF
+[Unit]
+Description=$pidname service
+After=cloud-init.service
+
+[Service]
+Type=forking
+User=admin
+PIDFile=/var/run/$companyname/$pidname.pid
+ExecStart=$servicedir/$pidname-start.sh
+ExecReload=
+ExecStop=$servicedir/$pidname-jar-stop.sh
+PrivateTmp=true
+
+[Install]
+WantedBy=multi-user.target
+EOF
+) > $servicedir/$pidname.service
+
+chmod 754 $servicedir/*
+
+
+sudo systemctl enable $servicedir/$pidname.service
+sudo systemctl start $pidname
