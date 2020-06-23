9eb6e3e21239601a7ec054d337fd1abe06abd0e4
niweiMac <niwei@yodoo.net.cn>
2020-06-23 16:14:42 +0800

    more test & comments

--- demo0623.java
+++ demo0623.java
@@ -2,15 +2,17 @@ public class demo0623 {
 
     public static void main(String[] args) {
         int input[]={1,3,7,9,3,3,5,6,7,3};
-        System.out.println(new demo0623().test(input, 7));
+
+        test(input, 3);
+        test(input, 7);
     }
 
-    public int test(int input[], int intVal){
+    public static int test(int input[], int intVal){
         int finalLength= input.length;
         for (int i=0; i< finalLength; i++){
             System.out.print(input[i]+" ");
         }
-        System.out.println("");
+        System.out.println("\nafter remove "+intVal+":");
         int j=0;
         for (int i=0; i< input.length; i++){
             if (input[i] == intVal){
@@ -23,7 +25,7 @@ public class demo0623 {
         for (int i=0; i< finalLength; i++){
             System.out.print(input[i]+" ");
         }
-        System.out.println("\nfinalLength: "+finalLength);
+        System.out.println("\nfinalLength: "+finalLength +"\n");
         return finalLength;
     }
 }
